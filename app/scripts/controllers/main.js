ed9c020edc4577502d754fec14824a25f6def202
Zainabed <zainabed@gmail.com>
2017-05-22 15:31:48 +0530

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name materialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the materialApp
+ */
+angular.module('materialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
